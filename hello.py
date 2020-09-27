78b86118933c7328a278bcd164a9b558bcdfb9e0
unknown <jean-pierre14>
2020-09-27 21:36:00 +0200

    Datatype

---
+++ hello.py
@@ -0,0 +1,27 @@
+print('Welcome to my first .py')
+
+x = 10
+y = 1.2
+sum = x + 5
+a, b, c = "number one", "number two", "number three"
+
+name = "awesome"
+code = "Python is "
+python = code + name
+
+
+def myFunc():
+    print("Python is " + name)
+
+
+myFunc()
+
+c = 3+3
+print(type(x))
+print(type(y))
+print(type(name))
+print(type(c))
+
+# print(python)
+# print(sum)
+# print(a, b,c)
