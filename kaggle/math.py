ece27029c351cf9e3c126f907a45c6cff0828a91
User <chiruzabisimwa@gmail.com>
2021-07-08 23:29:19 +0200

    math module

---
+++ kaggle/math.py
@@ -0,0 +1,16 @@
+import math
+# To import all variable in a module you do like this
+from math import *
+
+print("Hello math is here")
+print(type(math))
+# To see all the functions of the math module we'll use the dir keyword to get it
+
+print(dir(math))
+# If we need help, we do like so
+help(math.log)
+
+name = "BISIMWA"
+lastname = "GRACE"
+
+print(name, lastname)
